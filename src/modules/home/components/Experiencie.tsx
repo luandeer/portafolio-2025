a2d7ce8777de3e43f61efe62c627a8ec0dfd1d4e
alexander <alexander_pere@hotmail.com>
2025-10-18 15:46:34 -0500

    experience

--- src/modules/home/components/Experiencie.tsx
+++ src/modules/home/components/Experiencie.tsx
@@ -1,112 +1,70 @@
 'use client';
-import { useState } from 'react';
+
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from '@/common/components/ui/accordion';
 import Image from 'next/image';
+import events from '../data/experienceData.json';
 
 export default function Experiencie() {
-	const [showAll, setShowAll] = useState(false);
-
-	const events = [
-		{
-			year: '(noviembre 2024 – Febrero 2025)',
-			title: 'Nerd - Frontend Developer',
-			description:
-				'Desarrollé interfaces para e-commerce e inmobiliarias, asegurando su funcionalidad y preparación para el backend. Creé experiencias de usuario fluidas, diseñé plantillas de mailing en HTML/CSS y coordiné con equipos de diseño y QA. Además, gestioné repositorios en Git y GitHub para optimizar el trabajo colaborativo.',
-			image: '/nerd.jpg',
-			tags: [
-				'Html',
-				'css',
-				'javascript',
-				'tailwind css',
-				'Siteground',
-				'vercel',
-				'Git/GitHub',
-			],
-		},
-		{
-			year: '(julio 2024 – septiembre 2024)',
-			title: 'EDteam - Intership in Frontend Development',
-			description:
-				'Desarrollé componentes en React y Next.js, usando GitLab, CSSModules y Tailwind CSS. Participé en todo el ciclo de desarrollo, incluyendo features, bugs y refactorización según estándares de Edteam. Además, documenté procesos y realicé pruebas en desarrollo y producción para asegurar calidad y rendimiento.',
-			image: '/edteam.jpg',
-			tags: [
-				'React',
-				'Typescript',
-				'Next.js',
-				'Tailwind CSS',
-				'GitLab',
-				'Module css',
-				'React Query',
-				'Zustand',
-			],
-		},
-		{
-			year: '(abril 2023 – mayo 2024)',
-			title: 'Sonqu - Junior Software Engineer',
-			description:
-				'Desarrollé módulos para el dashboard de gestión interna, implementando cruds para la gestión de usuarios con TanStack Query para datos en tiempo real. Convertí el prototipo de Figma en código funcional, creando componentes interactivos y un diseño responsivo. También implementé autenticación y gestión de usuarios, colaborando en pruebas antes del despliegue. Además, trabajé en equipo para garantizar buenas prácticas de desarrollo y configuré el repositorio en GitHub para facilitar la colaboración.',
-			image: '/experiencia/sonqulogo.png',
-			tags: [
-				'React',
-				' Auth.js',
-				'Typescript',
-				'Next.js',
-				'Tailwind CSS',
-				'Github',
-				'Module css',
-				'React Query',
-				'Zustand',
-			],
-		},
-
-		// Puedes agregar más experiencias aquí...
-	];
-
-	// Mostrar solo los dos primeros eventos si showAll es false
-	const visibleEvents = showAll ? events : events.slice(0, 2);
-
 	return (
-		<div>
-			<div>
-				{visibleEvents.map((event, index) => (
-					<div key={index} className="py-2.5">
-						<div className="text-sm md:text-base flex items-start gap-3">
+		<Accordion type="single" collapsible>
+			{events.map((event, index) => (
+				<AccordionItem
+					key={index}
+					value={`item-${index}`}
+					className="border-b border-gray-800"
+				>
+					<AccordionTrigger className="hover:no-underline">
+						<div className="flex items-start gap-3 w-full">
 							<Image
 								src={event.image}
-								alt={event.title}
-								className="object-cover w-full max-w-[30px] h-[30px]  bg-white rounded-sm"
+								alt={event.company}
+								className="object-cover w-full max-w-[30px] h-[30px] bg-white rounded-sm border border-gray-800"
 								width={30}
 								height={30}
 							/>
-							<div className="flex flex-col">
-								<h3 className="text-xs mb-1 text-gray-300">{event.year}</h3>
-								<h3 className="font-semibold">{event.title}</h3>
-								<p className="text-sm text-gray-300">{event.description}</p>
-								{/* Etiquetas */}
-								<div className="flex flex-wrap gap-2 mt-2">
-									{event.tags.map((tag, i) => (
-										<span
-											key={i}
-											className="bg-gray-800 text-gray-300 text-xs px-2 py-1 rounded-md"
+							<div className="flex flex-col gap-0.5 w-full">
+								<div className="flex flex-wrap items-center gap-x-2 w-full">
+									<h3 className="font-semibold leading-none">{event.role}</h3>
+									{event.link ? (
+										<a
+											href={event.link}
+											target="_blank"
+											rel="noopener noreferrer"
+											className="font-medium text-blue-400 text-xs hover:underline"
 										>
-											{tag}
+											@{event.company}
+										</a>
+									) : (
+										<span className="font-medium text-gray-300 text-xs">
+											@{event.company}
 										</span>
-									))}
+									)}
 								</div>
+								<h3 className="text-xs mb-1 text-gray-400">{event.year}</h3>
 							</div>
 						</div>
-					</div>
-				))}
-			</div>
+					</AccordionTrigger>
 
-			{/* Botón para ver más o menos */}
-			{events.length > 2 && (
-				<button
-					onClick={() => setShowAll(!showAll)}
-					className="mt-4 text-blue-500 text-center text-sm w-full font-medium underline underline-offset-4 cursor-pointer"
-				>
-					{showAll ? 'Ver menos' : 'Ver más'}
-				</button>
-			)}
-		</div>
+					<AccordionContent>
+						<p className="text-sm text-gray-300">{event.description}</p>
+						<div className="flex flex-wrap gap-2 mt-2">
+							{event.tags.map((tag, i) => (
+								<span
+									key={i}
+									className="bg-gray-800 text-gray-300 text-xs px-2 py-1 rounded-md"
+								>
+									{tag}
+								</span>
+							))}
+						</div>
+					</AccordionContent>
+				</AccordionItem>
+			))}
+		</Accordion>
 	);
 }
